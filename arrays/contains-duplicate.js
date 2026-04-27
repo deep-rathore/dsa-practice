89daa743c5f2597105a52fecb15346cda5a946d9
Gx <rathore230788@gmail.com>
2026-04-27 18:21:03 +0530

    contains duplicate file added

---
+++ arrays/contains-duplicate.js
@@ -0,0 +1,12 @@
+function containsDuplicate(nums) {
+    const seen = new Set();
+    
+    for (let num of nums) {
+        if (seen.has(num)) {
+            return true;
+        }
+        seen.add(num);
+    }
+    
+    return false;
+}
